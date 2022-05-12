0508fb4a225823567f27224023a264641e48d7a4
Arash Mehdipour <arash201221@gmail.com>
2022-05-12 11:27:00 +0430

    Add project files.

---
+++ CSharpTypes.Classes/Fields.cs
@@ -0,0 +1,23 @@
+﻿namespace CSharpTypes.Classes
+{
+    public class Fields
+    {
+        /// <summary>
+        ///  فیلدها در کانستراکتور و یا همانجایی که تعریف می شوند مقداردهی می شوند 
+        /// </summary>
+        //private readonly int _age = 100;
+        private readonly int _age;
+        public Fields(int age)
+        {
+            _age = age;
+        }
+        /// <summary>
+        /// اگر در جای غیر از موارد بالا مقدار دهی شوند باعث ایجاد خطا می شویم
+        /// </summary>
+        public void ChangeAge()
+        { 
+            //_age = _age + 1;
+            Console.WriteLine(_age);
+        }
+    }
+}
---
+++ CSharpTypes.Classes/Program.cs
@@ -0,0 +1,7 @@
+﻿using CSharpTypes.Classes;
+
+Fields person = new Fields(1);
+person.ChangeAge();
+
+Console.WriteLine("Hello, World!");
+Console.ReadKey();
