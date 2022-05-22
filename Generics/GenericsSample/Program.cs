0bd839ea2393fbc8960b167cd8c616ebe35d9c45
Arash Mehdipour <arash201221@gmail.com>
2022-05-22 21:43:59 +0430

    Add Generics

---
+++ Generics/GenericsSample/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+
+
+using GenericsSample.Generics;
+
+#region GenericsSample
+GenericsExample<int> genericsInt = new();
+Console.WriteLine(genericsInt.GenericsConcat(1, 2));
+
+
+GenericsExample<string> genericsString = new();
+Console.WriteLine(genericsString.GenericsConcat("Arash", "Mehadipour"));
+
+GenericsExample<Person> genericsperson = new();
+
+Console.WriteLine(genericsperson.GenericsConcat(
+    new Person() { FirstName = "PersonName1", LastName = "PersonLastName1" },
+    new Person() { FirstName = "PersonName2", LastName = "PersonLastName2" }
+    ));
+
+#endregion
+
+
+Console.WriteLine("Hello, World!");
