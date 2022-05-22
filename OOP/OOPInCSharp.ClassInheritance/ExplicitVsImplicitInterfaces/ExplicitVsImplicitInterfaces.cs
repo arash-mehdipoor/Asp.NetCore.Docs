faad6c35f7d7cfa50de242f7768a38e317d0040c
Arash Mehdipour <arash201221@gmail.com>
2022-05-22 14:38:39 +0430

    add ExplicitVsImplicitInterfaces

---
+++ OOP/OOPInCSharp.ClassInheritance/ExplicitVsImplicitInterfaces/ExplicitVsImplicitInterfaces.cs
@@ -0,0 +1,58 @@
+﻿namespace OOPInCSharp.ClassInheritance.ExplicitVsImplicitInterfaces
+{
+    #region Description
+    /// <summary>
+    /// یک اینترنفیس رو میتونیم به دو روش مختلف پیاده سازی بکنیم  
+    /// Explicit یا Implicit
+    /// Implicit :  بصورت معمول
+    /// یعنی یک اینترفیس رو اسمشو مینویسیم و پیاده سازیش رو انجام میدیم و متدهایی که پیاده سازی میکنیم ازش پابلیک هستن
+    /// Explicit :
+    /// یعنی موقع پیاده سازی سطح دسترسیش رو برمیداریم برای اینکه 
+    /// Explicit
+    /// پیاده سازی بشه اسم اینترفیس رو قبل از متدم قرار میدم مثل :
+    /// interface.MethodName() {}
+    /// ویژگی هاش :
+    /// 
+    /// </summary> 
+    #endregion
+
+    #region Implicit
+    public interface ImplicitInterface
+    {
+        void Test();
+        void Temp();
+    }
+    public class ImplicitInterfaceClass : ImplicitInterface
+    {
+        public void Temp()
+        {
+            Console.WriteLine("Temp");
+        }
+
+        public void Test()
+        {
+            Console.WriteLine("Test");
+        }
+    } 
+    #endregion
+
+    #region Explicit
+    public interface ExplicitInterface
+    {
+        void Test();
+        void Temp();
+    }
+    public class ExplicitInterfaceClass : ExplicitInterface
+    {
+        void ExplicitInterface.Temp()
+        {
+            Console.WriteLine("Temp");
+        }
+
+        void ExplicitInterface.Test()
+        {
+            Console.WriteLine("Test");
+        }
+    } 
+    #endregion
+}
--- OOP/OOPInCSharp.ClassInheritance/Program.cs
+++ OOP/OOPInCSharp.ClassInheritance/Program.cs
@@ -1,8 +1,33 @@
 ﻿
 
 
-using OOPInCSharp.ClassInheritance.Inheritance;
+using OOPInCSharp.ClassInheritance.ExplicitVsImplicitInterfaces;
 using OOPInCSharp.ClassInheritance.Interface;
 
 
-using(MyDisposable testClassInterface = new MyDisposable()) { }
+#region IDisposable
+using (MyDisposable testClassInterface = new MyDisposable()) { }
+#endregion 
+
+#region ExplicitVsImplicitInterfaces
+// ImplicitInterface
+ImplicitInterfaceClass implicitInterfaceClass = new();
+implicitInterfaceClass.Temp();
+implicitInterfaceClass.Test();
+
+#region Description
+/// <summary>
+/// به چه دردی میخوره : 
+/// در شراطی مختلفی ممکنه که بخوام یک تایپی رو استفاده بکنم یک امپلیمنشن انجام بدم  یک پیاد هزی انجام بدم
+/// که این پیاده سازی من از یک اینترفیسی باشه  ولی کسی بخواد مستقیم از تایپ من استفاده بکنه نبینه که من 
+/// دقیقا چه پیاده سازی انجام دادم نتونه مستقیم دسترسی پیدا بکنه
+/// یا ممکنه امضاهای یکسانی در دو اینترفیسمختل داشته باشیم در اینصورت میایم اکسپلیسیت استفاده میکنیم 
+/// تا دقیقا بگیم از کدوم امضا توی کدوم اینترفیس باید استفاده کنیم
+/// </summary> 
+#endregion
+// ExplicitInterface
+ExplicitInterfaceClass explicitInterfaceClass = new();
+ExplicitInterface explicitInterface = explicitInterfaceClass;
+explicitInterface.Test();
+explicitInterface.Temp();
+#endregion
