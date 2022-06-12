c6571bdce3a7f36f47c33b98ce0d2ee997eafe9c
Arash Mehdipour <arash201221@gmail.com>
2022-06-12 20:19:13 +0430

    Add List

--- Collections/Collections/Program.cs
+++ Collections/Collections/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+
+using Collections.Lists;
+
+#region List
+
+ListSample list = new ListSample();
+
+list.PrintCapacity(); // 0
+list.PrintCount(); // 0
+
+list.AddMember("1");
+list.PrintCapacity(); // 4
+list.PrintCount(); // 1
+
+list.AddMember("2");
+list.PrintCapacity(); // 4
+list.PrintCount(); // 2
+
+list.AddMember("3");
+list.PrintCapacity(); // 4
+list.PrintCount(); // 3
+
+list.AddMember("4");
+list.PrintCapacity(); // 4
+list.PrintCount(); // 4
+
+
+list.AddMember("5");
+list.PrintCapacity(); // 8
+list.PrintCount(); // 5
+
+list.AddMember("6");
+list.PrintCapacity(); // 8
+list.PrintCount(); // 6
+
+#endregion
+
+Console.ReadKey();
 Console.WriteLine("Hello, World!");
