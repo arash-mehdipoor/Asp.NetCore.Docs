bc616bcda3a1273bf90d32c968c265acf389dd71
Arash Mehdipour <arash201221@gmail.com>
2022-06-13 20:17:42 +0430

    Add SortedDictionary

---
+++ Collections/Collections/SortedDictionary/SortedDictionary.cs
@@ -0,0 +1,17 @@
+﻿namespace Collections.SortedDictionary
+{
+    #region Description
+    /// <summary>
+    /// مث دیکشنری هستش یعنی کلید و مقدار میگیره ولی دیتا رو توی ساختار بی تری نگهداری میکنه
+    /// و بشدت برای جستجو میتونه پرفورمنس خوبی بهمون بده،ومرتب شده هم هست
+    /// تفاوتش فقط به دلیل استفاده از ساختار 
+    /// BTree هستش
+    /// به همین دلیل که از این ساختار داره استفاده میکنه میتونه توی جستجو بسیار پرفورمنس خوبی داشته باشه
+    /// </summary> 
+    #endregion
+    #region SortedDictionary
+    public class SortedDictionarySample
+    {
+    } 
+    #endregion
+}
