4c441a11d8e1918f7e97f93b013423bdcd712ae4
Arash Mehdipour <arash201221@gmail.com>
2022-06-16 19:12:45 +0430

    Add MetaData

---
+++ Attributes/Attribute.DiscoveringMetadata/Program.cs
@@ -0,0 +1,11 @@
+﻿using Attribute.DiscoveringMetadata;
+using Attribute.Domain.Models;
+
+MetaDataPrinter printer01 = new MetaDataPrinter(typeof(int));
+printer01.Print();
+
+
+MetaDataPrinter printer02 = new MetaDataPrinter(typeof(Person));
+printer02.Print();
+
+Console.WriteLine("Hello, World!");
