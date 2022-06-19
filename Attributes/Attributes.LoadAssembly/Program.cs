533f969fd4339a1b3c7f4ace259d7847852a9590
Arash Mehdipour <arash201221@gmail.com>
2022-06-19 12:55:25 +0430

    Add AssemblyLoading Add ConditinalCompiling

---
+++ Attributes/Attributes.LoadAssembly/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+using System.Reflection;
+
+
+#region AssemblyLoading
+var assembly = Assembly.LoadFrom(@"C:\Users\Arash\source\repos\arash-mehdipoor\Asp.NetCore.Docs\Attributes\Attribute.Domain\bin\Debug\net6.0\Attribute.Domain.dll");
+
+var types = assembly.GetTypes();
+Console.WriteLine($"*********************{assembly.FullName}*********************");
+foreach (var item in types)
+{
+    Console.WriteLine(item.Name);
+    //EmbeddedAttribute
+    //NullableAttribute
+    //NullableContextAttribute
+    //InternalPrinter
+    //Person
+    //PersonPrinter
+    //PersonProxyAttribute
+
+} 
+#endregion
+
+Console.WriteLine("Hello, World!");
